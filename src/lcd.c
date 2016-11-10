ab7b81dbeed5739bf1bceb3d603c49c2f66cd517
ponpon <pooruse@gmail.com>
2016-11-10 16:49:09 +0800

    drawing ok~

--- src/lcd.c
+++ src/lcd.c
@@ -22,6 +22,7 @@
 #define FUNCTION 0x20
 #define FUNCTION_DL 0x10
 #define FUNCTION_RE 0x04
+#define FUNCTION_G 0x02
 
 #define CGRAM 0x40
 #define CGRAM_ADDR_MASK 0x3F
@@ -38,7 +39,6 @@
 
 #define READ_DATA 0x300
 #define READ_DATA_MASK 0xFF
-
 
 /* extended instructions */
 #define STAND_BY 0x01
@@ -64,9 +64,30 @@
 #define GRAPHIC_RAM_ADDR 0x80
 #define GRAPHIC_RAM_ADDR_MASK 0x7F
 
+uint8_t font[][8] = {
+    {0x00,0x40,0x60,0x70,0x78,0x7C,0X7E,0X7F},
+    {0x00,0X7E,0X7C,0X78,0X70,0X60,0X40,0x00}, // UP
+    {0x00,0x44,0x44,0x5F,0x44,0x44,0X00,0X00},    
+    {0x00,0x10,0x38,0x7C,0xFE,0x00,0X00,0X00}, // +-	
+    {0x00,0xFE,0x7C,0x38,0x10,0x00,0X00,0X00}, // R
+    {0x00,0x04,0x0C,0x1C,0x3C,0x1C,0X0C,0X04}, // DOWN	
+    {0x00,0x40,0x60,0x70,0x78,0x70,0X60,0X40}, // UP
+    {0x00,0x00,0x3e,0x51,0x49,0x45,0x3e,0x00}, // 0  -0x30
+    {0x00,0x00,0x40,0x42,0x7f,0x40,0x40,0x00}, // 1 
+    {0x00,0x00,0x42,0x61,0x51,0x49,0x46,0x00}, // 2 
+    {0x00,0x00,0x21,0x41,0x45,0x4b,0x31,0x00}, // 3 
+    {0x00,0x00,0x18,0x14,0x12,0x7f,0x10,0x00}, // 4 
+    {0x00,0x00,0x27,0x45,0x45,0x45,0x39,0x00}, // 5
+    {0x00,0x00,0x3c,0x4a,0x49,0x49,0x30,0x00}, // 6
+    {0x00,0x00,0x01,0x71,0x09,0x05,0x03,0x00}, // 7 
+    {0x00,0x00,0x36,0x49,0x49,0x49,0x36,0x00}, // 8 
+    {0x00,0x00,0x06,0x49,0x49,0x29,0x1e,0x00}, // 9
+};
 
-static void lcd_graphic_clear(void);
-
+static void lcd_draw_font(int f, int x, int y);
+static void lcd_draw_8x8(uint8_t *but, int x, int y);
+static void lcd_draw_clear(void);
+static void lcd_set_addr(int x, int y);
 static uint8_t read(uint8_t command);
 static void write(uint32_t data);
 static void wait_busy(void);
@@ -76,42 +97,89 @@ static void send_byte(uint8_t data);
 int delay;
 
 void lcd_init(void){
-
     
+    //uint32_t dat = 0;
     spi_init();
-
+    
+    /*
+    while(1){	
+	gpio_toggle_debug_led();
+	write(dat);
+    }
+    */
+    
+    // make lcd run in the 8'bit mode
     write(FUNCTION | FUNCTION_DL);
-    wait_busy();
+    
+    // normal command mode
     write(FUNCTION | FUNCTION_DL);
-    wait_busy();
 
+    // open display
     write(DISPLAY | DISPLAY_D);
-    wait_busy();
 
-    write(CLEAR);
-    for(delay=0;delay<200000;delay++);
+    write(ENTRY);
 
     write(HOME);
-    wait_busy();
-
-    write(CURSOR | CURSOR_RL);
-    wait_busy();
-    write(DDRAM);
-    wait_busy();
-
-    write(ENTRY | ENTRY_ID);
-
-    lcd_graphic_clear();
-    write(DDRAM);
-    wait_busy();
-    write(WRITE | 0x30);
-    wait_busy();
     
+    // clear ddram (note: can't clear graphic ram)
+    write(CLEAR);
+    for(delay=0;delay<2000000;delay++);
 
+    lcd_draw_clear();
+
+    lcd_draw_font(0, 0, 0);
+    lcd_draw_font(1, 1, 0);
+    lcd_draw_font(2, 2, 0);
+    lcd_draw_font(3, 3, 0);
+    lcd_draw_font(4, 4, 0);
+    lcd_draw_font(5, 5, 0);
+    lcd_draw_font(6, 6, 0);
+    
+    /*
+    {
+	uint8_t buf[8] = {
+	    0xFF,0xFF,0xFF,0xFF,
+	    0xFF,0xFF,0xFF,0xFF,
+	};
+	lcd_draw_8x8(buf,0,2);
+    }
+    */
+    /*
+    lcd_set_addr(0,0);
+    write(WRITE | 0xFF);
+    write(WRITE | 0xFF);
+    */
+    
 }
 
-static void lcd_graphic_clear(void){
-   
+static void lcd_draw_font(int f, int x, int y)
+{
+    lcd_draw_8x8(font[f],x,y);
+}
+
+static void lcd_draw_8x8(uint8_t *buf, int x, int y){
+    int i;
+    for(i = 0; i < 8; i++){
+	lcd_set_addr(x,y+i);
+	write(WRITE | buf[i]);
+    }
+}
+
+static void lcd_draw_clear(void){
+    int i,j;
+    for(i = 0; i < 32; i++){
+	lcd_set_addr(0, i);
+	for(j = 0; j < 32; j++){
+	    write(WRITE | 0x0);
+	}
+    }
+}
+
+static void lcd_set_addr(int x, int y){
+    write(FUNCTION | FUNCTION_G | FUNCTION_RE | FUNCTION_DL);
+    write(GRAPHIC_RAM_ADDR | y);
+    write(GRAPHIC_RAM_ADDR | x);
+    write(FUNCTION | FUNCTION_DL);
 }
 
 /** @brief LCD instruction bit composition
@@ -122,27 +190,11 @@ static void lcd_graphic_clear(void){
  *  please refer the LCD datasheet VG12864Z-KBW-S15.pdf
  *  page 26 Timming Diagram of Serial Mode Data Transfer
  */
-
-static uint8_t read(uint8_t command){
-    
-    uint8_t cmd,high_byte,low_byte;
-
-    cmd = command;
-    cmd <<= 1;
-    cmd |= 0xF8;
-    
-    spi_tx(cmd);
-    high_byte = spi_rx();
-    low_byte = spi_rx();
-    return (high_byte & 0xF0) | ( (0xF0 & low_byte) >> 4);
-}
-
 static void write(uint32_t data){
     
     uint8_t cmd;
     uint8_t payload;
 
-    
     cmd = (uint8_t)(data >> 8);
     payload = (uint8_t)data;
 
@@ -150,6 +202,7 @@ static void write(uint32_t data){
     send_sync(cmd);
     send_byte(payload);
     spi_cs(0);
+    wait_busy();
 }
 
 static void wait_busy(){
@@ -164,7 +217,7 @@ static void wait_busy(){
     
 #else
     
-    for(delay = 0; delay < 2000; delay++);
+    for(delay = 0; delay < 3000; delay++);
 #endif
     
 }
